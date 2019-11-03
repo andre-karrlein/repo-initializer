8528788d8f4f64fe4689e9ecad083cd3d9733f51
Andre Karrlein <andre@karrlein.com>
2019-11-03 14:20:58 +0100

    add check if php binary is available

--- src/composer.go
+++ src/composer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os/exec"
 )
 
 func initComposer() error {
@@ -10,10 +11,27 @@ func initComposer() error {
 	if err != nil {
 		return errors.New(fmt.Sprintf("Can not download composer phar: %v", err))
 	}
+	php := checkIfPhpBinAvailable()
+	if !php {
+		return nil
+	}
+	err = initializeComposer()
+	if err != nil {
+		return errors.New(fmt.Sprintf("Can not initialize composer: %v", err))
+	}
 	return nil
 }
 
 func getComposer() error {
 	fileUrl := "https://getcomposer.org/download/1.9.1/composer.phar"
 	return DownloadFile("composer.phar", fileUrl)
+}
+
+func checkIfPhpBinAvailable() bool {
+	_, err := exec.LookPath("php")
+	return err == nil
+}
+
+func initializeComposer() error {
+	return errors.New("NO INIT METHOD")
 }
